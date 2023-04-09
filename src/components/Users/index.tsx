18a72c75fc8f8a1ac25f230e837d42a8928cec03
Alexandr Naum <mfq07k9cz@gmail.com>
2023-04-09 14:00:30 +0300

    created get response  on api

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,43 +1,11 @@
+import axios from 'axios'
+
 export const Users = (props: any) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: '1',
-        isFollow: true,
-        avatarSrc:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEdaR-n2haIJL-Ys0ZZKADyIdAzG_RUWX4sg&usqp=CAU',
-        fullName: 'Alex',
-        status: 'I am frontend developer',
-        location: { city: 'Zarechniy', country: 'Russia' }
-      },
-      {
-        id: '2',
-        isFollow: false,
-        avatarSrc:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEdaR-n2haIJL-Ys0ZZKADyIdAzG_RUWX4sg&usqp=CAU',
-        fullName: 'Ksu',
-        status: 'I am frontend developer',
-        location: { city: 'Penza', country: 'Russia' }
-      },
-      {
-        id: '3',
-        isFollow: true,
-        avatarSrc:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEdaR-n2haIJL-Ys0ZZKADyIdAzG_RUWX4sg&usqp=CAU',
-        fullName: 'Lev',
-        status: 'I am frontend developer',
-        location: { city: 'Berlin', country: 'Germany' }
-      },
-      {
-        id: '4',
-        isFollow: false,
-        avatarSrc:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEdaR-n2haIJL-Ys0ZZKADyIdAzG_RUWX4sg&usqp=CAU',
-        fullName: 'Alexandr',
-        status: 'I am frontend developer',
-        location: { city: 'Miami', country: 'USA' }
-      }
-    ])
+    axios.get('https://samyrai.free.beeceptor.com/users').then(response => {
+      debugger
+      props.setUsers([...response.data])
+    })
   }
 
   return props.users.map((user: any) => (
