e8039386899fa595e6273db8a2179ef0ba411e94
Alexandr Naum <mfq07k9cz@gmail.com>
2023-04-09 14:33:32 +0300

    remove side effect in Users component

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,37 +1,44 @@
 import axios from 'axios'
 
 export const Users = (props: any) => {
-  if (props.users.length === 0) {
-    axios.get('https://samyrai.free.beeceptor.com/users').then(response => {
-      debugger
-      props.setUsers([...response.data])
-    })
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get('https://samyrai.free.beeceptor.com/users').then(response => {
+        debugger
+        props.setUsers([...response.data])
+      })
+    }
   }
 
-  return props.users.map((user: any) => (
-    <div key={user.id}>
-      <span>
-        <div>
-          <img src={user.avatarSrc} alt={user.fullName} />
+  return (
+    <div>
+      <button onClick={getUsers}>Get users</button>
+      {props.users.map((user: any) => (
+        <div key={user.id}>
+          <span>
+            <div>
+              <img src={user.avatarSrc} alt={user.fullName} />
+            </div>
+            <div>
+              {user.isFollow ? (
+                <button onClick={() => props.unFollow(user.id)}>Unfollow</button>
+              ) : (
+                <button onClick={() => props.follow(user.id)}>Follow</button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{user.fullName}</div>
+              <div>{user.status}</div>
+            </span>
+            <span>
+              <div>Страна: {user.location.country}</div>
+              <div>Город: {user.location.city}</div>
+            </span>
+          </span>
         </div>
-        <div>
-          {user.isFollow ? (
-            <button onClick={() => props.unFollow(user.id)}>Unfollow</button>
-          ) : (
-            <button onClick={() => props.follow(user.id)}>Follow</button>
-          )}
-        </div>
-      </span>
-      <span>
-        <span>
-          <div>{user.fullName}</div>
-          <div>{user.status}</div>
-        </span>
-        <span>
-          <div>Страна: {user.location.country}</div>
-          <div>Город: {user.location.city}</div>
-        </span>
-      </span>
+      ))}
     </div>
-  ))
+  )
 }
