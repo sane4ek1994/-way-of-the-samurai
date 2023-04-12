a4e370a1116da7265e323bd4c087faab52bfbd16
Alexandr Naum <mfq07k9cz@gmail.com>
2023-04-12 23:22:57 +0300

    created pagination users

--- src/components/Users/usersClass.jsx
+++ src/components/Users/usersClass.jsx
@@ -1,16 +1,47 @@
 import React from 'react'
 import axios from 'axios'
 
+import styles from './users.module.css'
 export class Users extends React.Component {
   componentDidMount() {
-    axios.get('https://samyrai.free.beeceptor.com/users').then(response => {
+    axios
+      .get(`https://samyrai.free.beeceptor.com/users/${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers([...response.data.users])
+      })
+  }
+
+  onPageChanged = pageNumber => {
+    this.props.setCurrentPage(pageNumber)
+    axios.get(`https://samyrai.free.beeceptor.com/users/${pageNumber}&count=${this.props.pageSize}`).then(response => {
       this.props.setUsers([...response.data.users])
     })
   }
 
   render() {
+    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i)
+    }
+
     return (
       <div>
+        <div>
+          {pages.map(page => {
+            return (
+              <span
+                onClick={() => {
+                  this.onPageChanged(page)
+                }}
+                className={this.props.currentPage === page && styles.page_active}
+              >
+                {page}
+              </span>
+            )
+          })}
+        </div>
         {this.props.users.map(user => (
           <div key={user.id}>
             <span>
