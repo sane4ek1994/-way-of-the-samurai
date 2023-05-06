5fe3dddd2850c29123d106b20542ad14ac68ae16
Alexandr Naum <mfq07k9cz@gmail.com>
2023-05-06 12:33:13 +0300

    created getAuthUserData thunk

--- src/components/Header/headerContainer.jsx
+++ src/components/Header/headerContainer.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { authAPI } from '../../api/api'
 import { Header } from '.'
 import { withRouter } from '../../utils/withRouter'
 import { connect } from 'react-redux'
-import { setAuthUserData } from '../../redux/reducers/auth-Reducer'
+import { getAuthUserData } from '../../redux/reducers/auth-Reducer'
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    authAPI.me().then(response => {
-      if (response.data.resultCode === 0) {
-        let { id, email, login } = response.data.data
-        this.props.setAuthUserData(id, email, login)
-      }
-    })
+    this.props.getAuthUserData()
   }
   render() {
     return <Header {...this.props} />
@@ -24,4 +18,4 @@ let mapStateToProps = state => ({
   login: state.authReducer.login
 })
 
-export default connect(mapStateToProps, { setAuthUserData })(withRouter(HeaderContainer))
+export default connect(mapStateToProps, { getAuthUserData })(withRouter(HeaderContainer))
