d81eb3b6186e6880d85bd69c7c403b608495c5cb
Alexandr Naum <mfq07k9cz@gmail.com>
2023-05-07 18:36:38 +0300

    update hook

--- src/utils/withAuthRedirect.jsx
+++ src/utils/withAuthRedirect.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 export const withAuthRedirect = Component => {
-  class RedirectComponent extends React.Component {
+  class AuthRedirectComponent extends React.Component {
     render() {
       if (!this.props.isAuth) {
         return <Navigate to='/login' />
@@ -18,7 +18,7 @@ export const withAuthRedirect = Component => {
     }
   }
 
-  const ConnectedRedirectComponent = connect(mapStateToProps)(RedirectComponent)
+  const ConnectedRedirectComponent = connect(mapStateToProps)(AuthRedirectComponent)
 
   return ConnectedRedirectComponent
 }
