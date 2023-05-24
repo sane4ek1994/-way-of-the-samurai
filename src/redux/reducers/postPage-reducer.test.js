a88c83d8b4af97bc8cfadbc5868dca6a5b2174eb
Alexandr Naum <mfq07k9cz@gmail.com>
2023-05-24 05:43:40 +0300

    added state in test function

--- src/redux/reducers/postPage-reducer.test.js
+++ src/redux/reducers/postPage-reducer.test.js
@@ -31,6 +31,21 @@ test('New post shold be added, length of posts to be incremented', () => {
 test('After deleted length of message should be decrement', () => {
   // 1. Testing data
   let action = removePost(1)
+  let state = {
+    postData: [
+      {
+        id: '1',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK_YOcpPB1PxeJtAIyasitboh8RKkSFcCmNw&usqp=CAU',
+        text: 'Hello! I am Alex!'
+      },
+      { id: '2', src: 'https://img3.goodfon.ru/wallpaper/nbig/8/cd/spanch-bob-gubka-bob.jpg', text: 'QQ' },
+      {
+        id: '3',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxvwaJplxvK3UtOeCat2g-gJCt-xRwo1C8g&usqp=CAU',
+        text: 'What is your nature name?'
+      }
+    ]
+  }
 
   // 2. Action
   let newState = postReducer(state, action)
